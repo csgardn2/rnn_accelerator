60d9b2432433456415ec90c80e7239d549dee0d7
Conor Gardner <cgardner65536@yahoo.com>
2017-03-08 14:00:09 -0600

    Created skeliton hotspot rnn directory.

---
+++ tensorflow/hotspot/rnn_neural_code/hotspot_rnn.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3.5
+
+import sys
+import tensorflow
+import random
+from tensorflow.python.ops.rnn_cell import LSTMCell
+
+sys.path.append("./training_inputs");
+
+def main(_):
+    # TODO
+
+# This is actually the first thing that is executed.  It is hard-coded to
+# search for a function called 'main' and call it.
+tensorflow.app.run()
+
+
